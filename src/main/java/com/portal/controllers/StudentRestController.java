c4c7b9897d04451b75b8031f1d944a6481ba9c4f
cesar-medina <cesar-medina@bitbucket.org>
2022-06-21 00:58:12 -0500

    ANTES DE PRUEBAS

--- src/main/java/com/portal/controllers/StudentRestController.java
+++ src/main/java/com/portal/controllers/StudentRestController.java
@@ -3,6 +3,7 @@ package com.portal.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,15 @@ import com.portal.persistence.StudentServiceDb;
 
 @RestController
 @RequestMapping("/students")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class StudentRestController {
 	@Autowired
 	private StudentServiceDb studentServiceDb;
+	@GetMapping("/")
+	public String login() {
+		return "authenticated";
+	}
+	
 	@GetMapping("/all")
 	public List<Student> getAllStudentsDb(){
 		return studentServiceDb.getAllStudentsDb();
